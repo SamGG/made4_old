af3d4dc6df5ae3aeafdeeed34656a593fdf92000
Aedin Culhane <Aedin.Culhane@ucd.ie>
2007-04-11 00:00:17 +0000

    R cmd fix on graph1D and small update to vignette
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/made4@23747 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/graph1D.R
+++ R/graph1D.R
@@ -2,7 +2,7 @@
 function (dfx,  classvec=NULL, ax = 1, hor=FALSE, s.nam=row.names(dfx), n=NULL, scaled=TRUE, col="red", width=NULL, ...) {
 
     s.nam<-as.vector(s.nam)
-
+    
     if(!is.null(n)) {
       spec.ind<-genes1d(dfx, n=n)
       print(spec.ind)
@@ -17,32 +17,31 @@ function (dfx,  classvec=NULL, ax = 1, hor=FALSE, s.nam=row.names(dfx), n=NULL, 
 
     plot.new()
 
-    if (!is.vector(dfx)) dfx<- dfx[, ax]
-    if (scaled) dfx<-btt(dfx)
-
-    if (length(col)==1) col=rep(col, length(dfx))
+ 
+    if (is.null(classvec)) {
+	if (length(col)==1) col=as.character(rep(col, length(dfx)))
+	}
 
     if (!is.null(classvec)) {
-        cols=col           
+        cols=col
 	if (!length(cols) == length(levels(classvec))) {
           #print("Using default colours in getcol() as number of colours < the number of levels in classvec")
           cols = getcol(1:length(levels(classvec)))
           }
 
-	col = factor(classvec, labels = cols)
+	col = as.character(factor(classvec, labels = cols))
 
 	}
-
-    col = as.vector(col)
-
     
+    if (!is.vector(dfx)) dfx<- dfx[, ax]
+    if (scaled) dfx<-btt(dfx)
 
     if (is.null(width)) width=c(-2,1)
   
     margin=1
 
     if(!hor) {
-        par(usr = c(width[1],  width[2], floor(min(dfx))-margin, ceiling(max(dfx))+margin, ))
+        par(usr = c(width[1],  width[2], (floor(min(dfx))-margin), (ceiling(max(dfx))+margin)))
 
     	for (i in c(1:length(dfx))) {
         	points(0, dfx[i], col = col[i], ...)
@@ -58,7 +57,7 @@ function (dfx,  classvec=NULL, ax = 1, hor=FALSE, s.nam=row.names(dfx), n=NULL, 
 
     if(hor){
 
-        par(usr = c(floor(min(dfx))-margin, ceiling(max(dfx))+margin, width[1],  width[2]))
+        par(usr = c((floor(min(dfx))-margin), (ceiling(max(dfx))+margin), width[1], width[2]))
     	for (i in c(1:length(dfx))) {
   		points(dfx[i], 0, col = col[i], ...)
         	if (is.null(n)) text(dfx[i], -0.2, s.nam[i], adj = c(0, 0.5), srt = -90, ...)
