056545ba0e39ff15911913b4bcb1c5cb7888c804
Aedin Culhane <Aedin.Culhane@ucd.ie>
2007-09-19 00:10:40 +0000

    Added new function pretty.dend. This draws a dendrogram, a color bars for sample co-variates under the dendrogram.  Added new functionality to overview and to heatplot. Heatplot now uses heatmap.2 and includes a scale bar.  Also bug fixes to overview and heaplot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/made4@27260 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/heatplot.R
+++ R/heatplot.R
@@ -1,9 +1,10 @@
-heatplot<-function (dataset, dend = TRUE, lowcol = "green", highcol = "red", 
-    Colv = NULL, Rowv = NULL, cols.default = TRUE,scale="row",  classvec=NULL, ...) 
+heatplot<-function (dataset, dend = c("both", "row", "column", "none"),  cols.default = TRUE, lowcol = "green", highcol = "red", scale="row",  classvec=NULL, classvec2=NULL,  ...) 
 {
     library(gplots)
     data <- array2ade4(dataset)
     data <- as.matrix(data)
+ 
+
     distEisen <- function(x, use = "pairwise.complete.obs") {
         co.x <- cor(x, use = use)
         dist.co.x <- 1 - co.x
@@ -26,36 +27,71 @@ heatplot<-function (dataset, dend = TRUE, lowcol = "green", highcol = "red",
     if (cols.default) 
         plotcols = cols.gentleman()
     else plotcols = cols()
-    if (!dend) {
-        if (is.null(Colv)) 
-            Colv <- NA
-        if (is.null(Rowv)) 
-            Rowv <- NA
-    }
-    if (dend) {
-        if (is.null(Colv)) 
-            Colv <- as.dendrogram(hclust(distEisen(data), method = "ave"))
-        if (is.null(Rowv)) 
-            Rowv <- as.dendrogram(hclust(distEisen(t(data)), 
-                method = "ave"))
-    }
+
+    #dendrogram = c("both","row","column","none"),
+
+    # backward compliance
     
-    if (!is.null(classvec)) {
-       classvec=as.factor(classvec)
-       SideCols= factor(classvec, labels=getcol(length(levels(classvec))))
-       SideCols= as.character(SideCols)
-       nSC = length(SideCols)
-       if (!nSC %in% dim(data)) {
-           print("Can't use classvec, length not equal to nrow or ncol in data")  
-           heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", ...)
-           }
-           
-       if (nSC == nrow(data)) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", RowSideColors= SideCols,...)
+    
+    if (dend==TRUE) dend="both"
+    if (dend==FALSE) dend="none"
+
+    dend <- match.arg(dend)
+    
+    Colv <- FALSE
+    Rowv <- FALSE
+
+    dend=tolower(dend)
+
+    if (dend%in% c("row", "r")) Rowv= as.dendrogram(hclust(distEisen(t(data)), method = "ave"))
+    if (dend%in% c("column", "col", "c")) { 
+             Colv=as.dendrogram(hclust(distEisen(data), method = "ave"))
+             dend="column"}
+    if (dend%in% c("both","TRUE")) { 
+         Colv=as.dendrogram(hclust(distEisen(data), method = "ave"))
+         Rowv= as.dendrogram(hclust(distEisen(t(data)), method = "ave"))
+         dend="both"
+	}
+    
+
+    
+ 
+    RSideColors= CSideColors =NULL
+    if (any(!is.null(classvec), !is.null(classvec2))) {
        
-       if (nSC == ncol(data)) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none",  ColSideColors= SideCols,...)       
+       proc.classvec<-function(classvec){ 
+         classvec=as.factor(classvec)
+         SideCols= factor(classvec, labels=getcol(length(levels(classvec))))
+         SideCols= as.character(SideCols)
+         nSC = length(SideCols)
+         return(list(nSC, SideCols))
+	 }
+
+       if (!is.null(classvec)) {
+                out = proc.classvec(classvec)
+		nSC= out[[1]]
+                SideCols = out[[2]]
+		if (!nSC %in% dim(data)) print("Can't use classvec, length not equal to nrow or ncol in data")
+                if (nSC == nrow(data)) RSideColors=SideCols 
+		if (nSC == ncol(data)) CSideColors= SideCols
+		}
+       if (!is.null(classvec2)) {
+                out = proc.classvec(classvec2)
+		nSC= out[[1]]
+                SideCols = out[[2]]
+                if (!nSC %in% dim(data)) print("Can't use classvec2, length not equal to nrow or ncol in data")
+                if (nSC == nrow(data)) RSideColors= SideCols 
+		if (nSC == ncol(data)) CSideColors= SideCols
+		}
        
+       if (all(is.null(RSideColors),is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols,   scale=scale, trace="none",  density.info="none", dendrogram=dend,...)
+       if (all(!is.null(RSideColors),is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", RowSideColors= RSideColors,dendrogram=dend,...)
+       if (all(is.null(RSideColors),!is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none",  ColSideColors= CSideColors,dendrogram=dend,...)
+       if (all(!is.null(RSideColors),!is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", RowSideColors= RSideColors, ColSideColors= CSideColors, dendrogram=dend, ...)       
        }
     else 
-        heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", ...)
-        
+        heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", dendrogram=dend, ...)
+    
+
+
 }
--- R/overview.R
+++ R/overview.R
@@ -1,62 +1,62 @@
-"overview"<-
-function(dataset, labels=NULL, title="", classvec=NULL, hc=TRUE, boxplot=TRUE, hist=TRUE){
+overview<- function(dataset, labels = NULL, title = "", classvec = NULL, hc = TRUE, boxplot = TRUE, hist = TRUE, returnTree=FALSE) 
+{
 
-        cols=NULL
-        layout(matrix(c(1,1, 2,3), 2, 2, byrow = TRUE))
-        if (is.null(labels)) labels=colnames(dataset)
-        if (!is.null(labels)) labels=as.character(labels)
-        if (!is.null(classvec))  {
-           classvec= as.factor(classvec)
-           cols= getcol(as.numeric(classvec))
-           #print(cols)
-           layout(matrix(c(1,1,2,2, 3,4), 3, 2, byrow = TRUE), heights=c(1.5,0.1,2))
-            }
-        distEisen <- function(x, use="pairwise.complete.obs") {
-          co.x <- cor(x, use=use)
-          dist.co.x <- 1-co.x
-          return(as.dist(dist.co.x)) 
-          }
-        
-         colhc <- function(hc, classvec) {
-          margins= par()$mar
-         # print(margins)
-          par(mar = c(0.5, margins[1], 0, margins[2]))
-          nc= length(as.character(classvec))
-          colInd = hc$order
-          image(cbind(1:nc),  col = cols[colInd], axes = FALSE)
-          par(mar = margins)
-          }
-
-         
-         if (hc) {
-           	  if (!inherits(dataset, "AffyBatch")) {dataset <- array2ade4(dataset, trans = FALSE)}
-
-		  if (inherits(dataset, "AffyBatch")) {dataset  =exprs(dataset)}
-
-		  hc=hclust(distEisen(dataset), method="ave")
-                   plot(hc, hang=-1, labels=labels, main=paste("Histogram", title, sep=" "))  # cor dist and average linkage 
-                   if (!missing(classvec)) colhc(hc, classvec) 
-                   }
-
-          if (boxplot) {
-	  	  if (!inherits(dataset, "AffyBatch")) {dataset <- array2ade4(dataset, trans = FALSE)}
-
-		  boxplot(dataset, main=paste("boxplot", title, sep=" "), names=labels,par(las=2), col=cols)
-		}
-
-          if (hist) {
-		  if (!inherits(dataset, "AffyBatch")) {
-			dataset <- array2ade4(dataset, trans = FALSE)
-		  	dataset = as.matrix(dataset)
-			}
-
-		  hist(dataset,xlab="", main=paste("Histogram", title, sep=" "))
-		}
- 
+	# Need to update this in made4. Then this will be removed from here and just source the Bioc one.
+    cols = NULL
+    if (sum(boxplot, hist, hc)==3) layout(matrix(c(1, 1, 2, 3), 2, 2, byrow = TRUE))
+    if (sum(boxplot, hist, hc)==2) layout(matrix(c(1, 1, 2, 2), 2, 2, byrow = TRUE))
+    
+  
+    if (is.null(labels)) 
+        labels = colnames(dataset)
+    if (!is.null(labels)) 
+        labels = as.character(labels)
+    if (!is.null(classvec)) {
+        cols = getcol(as.numeric(classvec))
+        layout(matrix(c(1, 1, 2, 2, 3, 4), 3, 2, byrow = TRUE), 
+            heights = c(1.5, 0.1, 2))
+    }
+    distEisen <- function(x, use = "pairwise.complete.obs") {
+        co.x <- cor(x, use = use)
+        dist.co.x <- 1 - co.x
+        return(as.dist(dist.co.x))
+    }
+    colhc <- function(hc, classvec) {
+        margins = par()$mar
+        par(mar = c(0.5, margins[1], 0, margins[2]))
+        nc = length(as.character(classvec))
+        colInd = hc$order
+        image(cbind(1:nc), col = cols[colInd], axes = FALSE)
+        par(mar = margins)
+    }
+    if (hc) {
+        if (!inherits(dataset, "AffyBatch")) {
+            dataset <- array2ade4(dataset, trans = FALSE)
         }
-
-
-
-
-
-
+        if (inherits(dataset, "AffyBatch")) {
+            dataset = exprs(dataset)
+        }
+        hc = hclust(distEisen(dataset), method = "ave")
+        plot(hc, hang = -1, labels = labels, main = paste("Histogram", 
+            title, sep = " "), sub="", xlab="")
+        if (!missing(classvec)) 
+            colhc(hc, classvec)
+    }
+    if (boxplot) {
+        if (!inherits(dataset, "AffyBatch")) {
+            dataset <- array2ade4(dataset, trans = FALSE)
+        }
+        boxplot(dataset, main = paste("boxplot", title, sep = " "), 
+            names = labels, par(las = 2), col = cols)
+    }
+    if (hist) {
+        if (!inherits(dataset, "AffyBatch")) {
+            dataset <- array2ade4(dataset, trans = FALSE)
+            dataset = as.matrix(dataset)
+        }
+        hist(dataset, xlab = "", main = paste("Histogram", title, 
+            sep = " "))
+    }
+    
+    if (returnTree) return(hc)
+}
