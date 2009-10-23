74495bb5ea640969cfb7421b8c6acd86edef5abb
Aedin Culhane <Aedin.Culhane@ucd.ie>
2009-10-23 17:03:03 +0000

    bug fix and update to heatplot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/made4@42598 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/heatplot.R
+++ R/heatplot.R
@@ -1,10 +1,22 @@
-heatplot<-function (dataset, dend = c("both", "row", "column", "none"),  cols.default = TRUE, lowcol = "green", highcol = "red", scale="row",  classvec=NULL, classvec2=NULL,  ...) 
+heatplot<-function (dataset, dend = c("both", "row", "column", "none"),
+    cols.default = TRUE, lowcol = "green", highcol = "red", scale = "none",
+    classvec = NULL, classvecCol=NULL, classvec2 = NULL,distfun=NULL, returnSampleTree=FALSE,method="ave", dualScale=TRUE, zlim=c(-3,3),scaleKey=TRUE, ...)
 {
     library(gplots)
     data <- array2ade4(dataset)
     data <- as.matrix(data)
- 
 
+    if (dualScale) { 
+      # print(range(data))
+       print(paste("Data (original) range: ", round(range(data),2)[1], round(range(data),2)[2]), sep="")
+       
+       data  <- t(scale(t(data)))
+       print(paste("Data (scale) range: ", round(range(data),2)[1], round(range(data),2)[2]), sep="")
+       
+       data  <- pmin(pmax(data,zlim[1]),zlim[2])
+
+       print(paste("Data scaled to range: ", round(range(data),2)[1], round(range(data),2)[2]), sep="")
+       }
     distEisen <- function(x, use = "pairwise.complete.obs") {
         co.x <- cor(x, use = use)
         dist.co.x <- 1 - co.x
@@ -12,9 +24,9 @@ heatplot<-function (dataset, dend = c("both", "row", "column", "none"),  cols.de
     }
     cols <- function(low = lowcol, high = highcol, ncolors = 123) {
         low <- col2rgb(low)/255
-        if (is.character(high)) 
+        if (is.character(high))
             high <- col2rgb(high)/255
-        col <- rgb(seq(low[1], high[1], len = ncolors), seq(low[2], 
+        col <- rgb(seq(low[1], high[1], len = ncolors), seq(low[2],
             high[2], len = ncolors), seq(low[3], high[3], len = ncolors))
         return(col)
     }
@@ -23,75 +35,101 @@ heatplot<-function (dataset, dend = c("both", "row", "column", "none"),  cols.de
         hmcol <- colorRampPalette(brewer.pal(10, "RdBu"))(256)
         return(rev(hmcol))
     }
-
-    if (cols.default) 
+    if (cols.default)
         plotcols = cols.gentleman()
     else plotcols = cols()
+    #if (dend == TRUE)
+    #    dend = "both"
+    #if (dend == FALSE)
+    #    dend = "none"
 
-    #dendrogram = c("both","row","column","none"),
+    if (is.null(distfun)) distf=distEisen
+    else distf = function(x) dist(t(x), method=distfun)
 
-    # backward compliance
-    
-    
-    if (dend==TRUE) dend="both"
-    if (dend==FALSE) dend="none"
-
-    dend <- match.arg(dend)
-    
+   ## Dendrogram column and row order
     Colv <- FALSE
     Rowv <- FALSE
+    dend <- match.arg(dend)
+    dend = tolower(dend)
 
-    dend=tolower(dend)
+  #  print(distf(data))
+    if (dend %in% c("row", "r")) {
+        print(dend)
+        Rowv = as.dendrogram(hclust(distf(t(data)), method = method))
+        }
 
-    if (dend%in% c("row", "r")) Rowv= as.dendrogram(hclust(distEisen(t(data)), method = "ave"))
-    if (dend%in% c("column", "col", "c")) { 
-             Colv=as.dendrogram(hclust(distEisen(data), method = "ave"))
-             dend="column"}
-    if (dend%in% c("both","TRUE")) { 
-         Colv=as.dendrogram(hclust(distEisen(data), method = "ave"))
-         Rowv= as.dendrogram(hclust(distEisen(t(data)), method = "ave"))
-         dend="both"
-	}
-    
+    if (dend %in% c("column", "col", "c")) {
+        print(dend)
+        Colv = as.dendrogram(hclust(distf(data), method = method))
+        dend = "column"
+        }
 
-    
- 
-    RSideColors= CSideColors =NULL
+    if (dend %in% c("both", "TRUE")) {
+        Colv = as.dendrogram(hclust(distf(data), method = method))
+        Rowv = as.dendrogram(hclust(distf(t(data)), method = method))
+        dend = "both"
+        }
+
+    ## Side color bars
+    RSideColors = CSideColors = NULL
     if (any(!is.null(classvec), !is.null(classvec2))) {
-       
-       proc.classvec<-function(classvec){ 
-         classvec=as.factor(classvec)
-         SideCols= factor(classvec, labels=getcol(length(levels(classvec))))
-         SideCols= as.character(SideCols)
-         nSC = length(SideCols)
-         return(list(nSC, SideCols))
-	 }
-
-       if (!is.null(classvec)) {
-                out = proc.classvec(classvec)
-		nSC= out[[1]]
-                SideCols = out[[2]]
-		if (!nSC %in% dim(data)) print("Can't use classvec, length not equal to nrow or ncol in data")
-                if (nSC == nrow(data)) RSideColors=SideCols 
-		if (nSC == ncol(data)) CSideColors= SideCols
-		}
-       if (!is.null(classvec2)) {
-                out = proc.classvec(classvec2)
-		nSC= out[[1]]
-                SideCols = out[[2]]
-                if (!nSC %in% dim(data)) print("Can't use classvec2, length not equal to nrow or ncol in data")
-                if (nSC == nrow(data)) RSideColors= SideCols 
-		if (nSC == ncol(data)) CSideColors= SideCols
-		}
-       
-       if (all(is.null(RSideColors),is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols,   scale=scale, trace="none",  density.info="none", dendrogram=dend,...)
-       if (all(!is.null(RSideColors),is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", RowSideColors= RSideColors,dendrogram=dend,...)
-       if (all(is.null(RSideColors),!is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none",  ColSideColors= CSideColors,dendrogram=dend,...)
-       if (all(!is.null(RSideColors),!is.null(CSideColors))) heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", RowSideColors= RSideColors, ColSideColors= CSideColors, dendrogram=dend, ...)       
+        proc.classvec <- function(classvec) {
+            classvec = as.factor(classvec)
+            if (is.null( classvecCol))  classvecCol=getcol(length(levels(classvec)))
+            #print(classvec)
+           
+            SideCols = factor(classvec, labels = classvecCol)
+            print(cbind(Class=levels(classvec), Color=levels(SideCols)))
+            SideCols = as.character(SideCols)
+            #print(SideCols)
+            nSC = length(SideCols)
+            return(list(nSC, SideCols))
+        }
+        if (!is.null(classvec)) {
+            out = proc.classvec(classvec)
+            nSC = out[[1]]
+            SideCols = out[[2]]
+            if (!nSC %in% dim(data))
+                print("Error: classvec length not equal to nrow or ncol in data")
+            if (nSC == nrow(data))
+                RSideColors = SideCols
+            if (nSC == ncol(data))
+                CSideColors = SideCols
+        }
+        if (!is.null(classvec2)) {
+            out = proc.classvec(classvec2)
+            nSC = out[[1]]
+            SideCols = out[[2]]
+            if (!nSC %in% dim(data))
+                print("Error: classvec2 length not equal to nrow or ncol in data")
+            if (nSC == nrow(data))
+                RSideColors = SideCols
+            if (nSC == ncol(data))
+                CSideColors = SideCols
+        }
        }
-    else 
-        heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols, scale=scale, trace="none",  density.info="none", dendrogram=dend, ...)
-    
 
 
+      ## NEED TO ADD BACK Dendrogram parameters
+        if (all(is.null(RSideColors), is.null(CSideColors)))
+            heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols,
+                scale = scale, trace = "none", density.info = "none", zlim=zlim,
+                dendrogram = dend, ...)
+        if (all(!is.null(RSideColors), is.null(CSideColors)))
+            heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols,
+                scale = scale, trace = "none", density.info = "none",
+                RowSideColors = RSideColors, dendrogram = dend,zlim=zlim,
+                ...)
+        if (all(is.null(RSideColors), !is.null(CSideColors)))
+            heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols,
+                scale = scale, trace = "none", density.info = "none",
+                ColSideColors = CSideColors, dendrogram = dend,zlim=zlim,
+                ...)
+        if (all(!is.null(RSideColors), !is.null(CSideColors)))
+            heatmap.2(data, Colv = Colv, Rowv = Rowv, col = plotcols,
+                scale = scale, trace = "none", density.info = "none",
+                RowSideColors = RSideColors, ColSideColors = CSideColors,zlim=zlim,
+                dendrogram = dend, ...)
+   
+   if (returnSampleTree)  return(Colv)
 }
